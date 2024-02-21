ccf6a6354174c46213d4b6bafbec8ef03e2eebf0
kukkalavandhu220 <150996362+kukkalavandhu220@users.noreply.github.com>
2024-02-21 17:09:14 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,234 @@
+bool(None)
+print(10)
+type(10)
+print(type(10))
+A=10
+B=10.0
+print(id(A))
+print(id(B))
+a=10
+b=20
+c=a+b
+d=a-b
+e=a*b
+q=a/b
+w=a%5
+u=a//b
+s=a**b
+k=a++b
+print(c,d,e,q,w,u,s)
+And=10
+n=2**2
+print(n)
+m=2**3
+print(m)
+l=2**4
+print(l)
+print("vandana"*5)
+o=10/2
+print(o)
+g=11/2
+print(g)
+f=11//2
+print(f)
+Q=10.5/2
+print(Q)
+W=10.5//2
+print(W)
+E=10.0/2
+print(E)
+R=10.0//2
+print(R)
+a=10
+b=20
+c=30
+d=40
+if a==10 and b==20:
+    print("available")
+else:
+    print("not available")
+
+if c==30 and d==40:
+    print("available")
+else:
+    print("not available")
+
+if a==10 and c==30:
+    print("available")
+else:
+    print("not available")
+        
+if a==10 and d==40:
+    print("available")
+else:
+    print("not available")
+
+if b==10 and c==20:
+    print("available")
+else:
+    print("not available")
+
+if b==10 and d==20:
+    print("available")
+else:
+    print("not available")
+if a==10 or b==20:
+    print("available")
+else:
+    print("not available")
+if a==10 or c==20:
+    print("available")
+else:
+    print("not available")
+if a==10 or d==20:
+    print("available")
+else:
+    print("not available")
+if b==10 or c==20:
+    print("available")
+else:
+    print("not available")
+if b==10 or d==20:
+    print("available")
+else:
+    print("not available")
+if c==10 or d==20:
+    print("available")
+else:
+    print("not available")
+if a==10 != b==20:
+    print("available")
+else:
+    print("not available")
+print(not(a))
+a=0
+print(a==10)
+print(a!=10)
+print(not b)
+if a:
+    print("a is with some value")
+if not a:
+    print("ais none")
+else:
+    print("invalid")
+print(bool(20))
+print(bool(30))
+print(bool(50))
+print(bool(None))
+print(bool(0))
+print(bool("Vandana"))
+a=10
+b=10
+print(a==b)
+print(a is b)
+a+=10
+print(a)
+a-=10
+print(a)
+a/=2
+print(a)
+a*=10
+print(a)
+a**=5
+print(a)
+a%=6
+print(a)
+a//=2
+print(a)
+a+=10
+
+a=20
+a+=20
+print(a)
+a-=20
+print(a)
+a*=20
+print(a)
+a**=20
+print(a)
+a/=20
+print(a)
+a//=20
+print(a)
+a%=20
+print(a)
+print(a==b)
+print(a!=b)
+print(a++b)
+print(a++a)
+print(a--a)
+print(a--b)
+print(a+-b)
+print(a+-a)
+print(a-+b)
+print(a-+a)
+print(a*-b)
+print(a*-a)
+print(a*+b)
+print(a*+a)
+print(a/+a)
+print(a/+b)
+print(a/-a)
+print(a/-b)
+print(a//b)
+print(a//a)
+print(a%a)
+print(a%b)
+print(a>=b)
+print(a<=b)
+print(a>=a)
+print(a<=a)
+print(a,b)
+print(-a,b)
+print(a,-b)
+print(+a,b)
+print(a,None)
+a=3
+print(a)
+print(a==b)
+print(a!=b)
+print(bool(a))
+print(bool(b))
+print("vandana"*10)
+print(bool(None))
+print(bool(0))
+print(bool("python"))
+print(a<b)
+print(a>b)
+print(bool(a<b))
+print(bool(a>b))
+print(a**9)
+print(b**5)
+print(a&b)
+print(a^b)
+print((a+b)**7)
+print((a-b)**a)
+if a==3 and b==10:
+    print('true')
+else:
+    print('false')
+if bool(a)!=bool(b):
+    print('available')
+else:
+    print('unavailable')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+    
